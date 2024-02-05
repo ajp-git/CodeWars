349aead03df6d852af8ca3608c7f03435ae92793
Alain Jean PARES <ajp@free.fr>
2024-02-05 10:08:42 +0100

    starting interpreter

---
+++ 5kyu/My_smallest_code_interpreter/target/debug/deps/My_smallest_code_interpreter-3078abac4ddceacd.d
@@ -0,0 +1,5 @@
+/home/ajp/kDrive/Rust/CodeWars/5kyu/My_smallest_code_interpreter/target/debug/deps/libMy_smallest_code_interpreter-3078abac4ddceacd.rmeta: src/main.rs
+
+/home/ajp/kDrive/Rust/CodeWars/5kyu/My_smallest_code_interpreter/target/debug/deps/My_smallest_code_interpreter-3078abac4ddceacd.d: src/main.rs
+
+src/main.rs:
