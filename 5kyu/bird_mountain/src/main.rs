3f66ae2294d6df38e20e882277d78c56c4feeef5
Alain Jean PARES <ajp@free.fr>
2024-07-04 11:24:01 +0200

    Removed .gitignore file

--- 5kyu/bird_mountain/src/main.rs
+++ 5kyu/bird_mountain/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn peak_height(mountain: &[&str]) -> u32 {
+    todo!();
+}
+// Add your tests here.
+// See https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
+
+#[cfg(test)]
+mod sample_tests {
+    use super::peak_height;
+
+    #[test]
+    fn example() {
+        let mountain = [
+            "^^^^^^        ",
+            " ^^^^^^^^     ",
+            "  ^^^^^^^     ",
+            "  ^^^^^       ",
+            "  ^^^^^^^^^^^ ",
+            "  ^^^^^^      ",
+            "  ^^^^        "
+        ];
+        assert_eq!(peak_height(&mountain), 3, "\nYour result (left) did not match expected result (right)");
+    }
+}
